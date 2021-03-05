e175b0b288fb7c9d70a69cf3be23dfb4cf082738
kicass8 <61785047+kicass8@users.noreply.github.com>
2021-03-05 19:32:47 +0100

    Update SixQuiPrend.java

--- java/SixQuiPrend.java
+++ java/SixQuiPrend.java
@@ -5,9 +5,21 @@ public class SixQuiPrend {
     private static final int NB_CARD_MAX = 104;
 
     public static int findNumberOfBullHeads(int value) {
-        // TODO
+        int result = 0;
+        if (value%5 == 0 && !(value%10 == 0))
+            result += 2;
+        if (value%10 == 0)
+            result += 3;
+        if (value >= 10) {
+            if(value > 100 && value%10 == ((value/10)%10)) {
+                result += 5;
+            }else if(value < 100 && value%10 == value/10)
+                result += 5;
+        }
+        if (result == 0)
+            result = 1;
 
-        return 0;
+        return result;
     }
 
     public static void gameLoop() {
